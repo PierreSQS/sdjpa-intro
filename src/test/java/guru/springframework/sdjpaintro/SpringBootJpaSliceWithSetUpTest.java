1a256d5f6ef60739beb910f7b3e0b154ba8aacb4
PierreSQS <nomo_messina@web.de>
2021-12-03 18:32:55 +0100

    Sec5-Chap27: added a new Test and commented some steps in a test

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaSliceWithSetUpTest.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaSliceWithSetUpTest.java
@@ -2,6 +2,7 @@ package guru.springframework.sdjpaintro;
 
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,23 +14,29 @@ import static org.assertj.core.api.Assertions.assertThat;
  *  Created by Pierrot on 11/24/2021
  */
 @DataJpaTest
-class SpringBootJpaTestSlice {
+class SpringBootJpaSliceWithSetUpTest {
 
     @Autowired
     BookRepository bookRepository;
+
+    @BeforeEach
+    void setUp() {
+        bookRepository.save(new Book("Mon Livre","75757ad-3t","Mon édition"));
+    }
 
     @Test
     void testBookRepository() {
         long countBefore = bookRepository.count();
 
-        // no data in the DB since no Customer saved
-        // either in Setup() or in @Configuration Class
-        assertThat(countBefore).isZero();
+        // will fail since 1 Book saved in Setup()
+        // assertThat(countBefore).isZero();
+        assertThat(countBefore).isEqualTo(1);
 
         bookRepository.save(new Book("Mon livre","123456","Mes editions"));
 
         long countAfter = bookRepository.count();
 
-        assertThat(countAfter).isEqualTo(1L);
+        // now 2 Book since 1 Book saved in SetUp()
+        assertThat(countAfter).isEqualTo(2L);
     }
 }
