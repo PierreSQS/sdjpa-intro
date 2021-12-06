bf56ac6fc27955e1213b4b66caec8374fc1aefa7
PierreSQS <nomo_messina@web.de>
2021-12-06 21:35:46 +0100

    Sec5-Chap37: introduced an AWS profile

---
+++ src/test/java/guru/springframework/sdjpaintro/MySQLAWSIntegrationTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.sdjpaintro;
+
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * Created by jt on 7/4/21.
+ */
+@ActiveProfiles("aws")
+@DataJpaTest
+@ComponentScan(basePackages = {"guru.springframework.sdjpaintro.bootstrap"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MySQLAWSIntegrationTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testMySQLOnAws() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(2);
+
+    }
+
+}
+
+
